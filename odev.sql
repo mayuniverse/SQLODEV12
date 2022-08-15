b2971cd68f4b60da8ec468b0274d3c3f323bd367
Muhammet Ali Yılmaz <81625252+mayuniverse@users.noreply.github.com>
2022-08-15 15:53:26 +0300

    Update odev.sql

---
+++ odev.sql
@@ -0,0 +1,27 @@
+-- film tablosunda film uzunluğu length sütununda gösterilmektedir. Uzunluğu ortalama film uzunluğundan fazla kaç tane film vardır?
+SELECT COUNT(*) FROM film
+WHERE length > 
+    (SELECT AVG(LENGTH) FROM film);
+
+-- film tablosunda en yüksek rental_rate değerine sahip kaç tane film vardır?
+SELECT COUNT(*) FROM film
+WHERE rental_rate = 
+(SELECT MAX(rental_rate) FROM film);
+
+-- film tablosunda en düşük rental_rate ve en düşün replacement_cost değerlerine sahip filmleri sıralayınız.
+SELECT * FROM film
+WHERE rental_rate = 
+(
+SELECT MIN(rental_rate) FROM film
+) 
+AND
+replacement_cost = 
+(
+SELECT MIN(replacement_cost) FROM film
+);
+
+-- payment tablosunda en fazla sayıda alışveriş yapan müşterileri(customer) sıralayınız.
+SELECT payment.amount, customer.customer_id, customer.first_name, customer.last_name FROM payment JOIN customer ON customer.customer_id = payment.customer_id
+WHERE payment.amount = (
+SELECT MAX(amount) FROM payment
+);
